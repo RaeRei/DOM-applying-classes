db8f5d1bc64c329ef7bade52098dca7f419f7126
Raemon Matenee <rmatene@xtra.co.nz>
2019-07-15 12:53:33 +1200

    Dom challenge1

--- classes.js
+++ classes.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -20,6 +20,22 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
+
 // CREATE FUNCTION three HERE
 
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('black')
+
+
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var makeVisible = document.getElementsByClassName("invisible")[0]
+  invisible.classList(visible)
+  console.log(visible)
+  
+}
